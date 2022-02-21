8b1c00d4dfa8dfa84fb68165ade57f582843f023
Predic <55022230+minchan02@users.noreply.github.com>
2022-02-21 16:44:11 +0900

    Create ObjectPointer.cpp

---
+++ C++/Chapter08/ObjectPointer.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Person
+{
+public:
+	void Sleep() { cout << "Sleep" << endl; }
+};
+
+class Student : public Person
+{
+public:
+	void Study() { cout << "Study" << endl; }
+};
+
+class PartTimeStudent : public Student
+{
+public:
+	void Work() { cout << "Work" << endl; }
+};
+
+int main(void) {
+	Person * ptr1 = new Student();
+	Person * ptr2 = new PartTimeStudent();
+	Student* ptr3 = new PartTimeStudent();
+	ptr1->Sleep();
+	ptr2->Sleep();
+	ptr3->Study();
+	delete ptr1; delete ptr2; delete ptr3; // delete로 메모리 삭제
+	return 0;
+}
+
+// Person형 포인터 변수는 Person객체를 상속하는 객체에 접근 가능하다.
